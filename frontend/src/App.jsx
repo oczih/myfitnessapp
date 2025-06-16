b8053ca1b9408316ce51640b002cd4d8c2dffec0
oczih <arvo.matilainen@gmail.com>
2025-06-16 11:36:14 +0300

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,15 @@ import {
   Routes, Route, Link,
   useMatch, useNavigate, Navigate
 } from 'react-router-dom'
-
+import { LoginElement } from './components/LoginElement'
 
 const App = () => {
   return(
     <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
       <h1>Gamify your habits!</h1>
+      <Routes>
+        <Route path="/login" element={<LoginElement />}></Route>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/LoginElement.jsx
@@ -0,0 +1,7 @@
+
+
+export const LoginElement = () => {
+    return (
+        <div></div>
+    )
+}
